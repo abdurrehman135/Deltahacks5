8dbc36c70e24871ca105d2f384487b173c313686
AllegraChen <allegra.chen@mail.utoronto.ca>
2019-01-26 16:14:17 -0500

    change the name into createdEventsList generate getters and setters

--- app/src/main/java/com/example/hsport/deltahacks5/deltahacks5/classes/User.java
+++ app/src/main/java/com/example/hsport/deltahacks5/deltahacks5/classes/User.java
@@ -6,10 +6,44 @@ public class User {
     private String userName;
     private String password;
     private ArrayList eventList;
-    private ArrayList createdEvents;
+    private ArrayList createdEventsList;
 
     public User(String userName, String password) {
         this.userName = userName;
         this.password = password;
     }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public ArrayList getEventList() {
+        return eventList;
+    }
+
+    public void setEventList(ArrayList eventList) {
+        this.eventList = eventList;
+    }
+
+    public ArrayList getCreatedEventsList() {
+        return createdEventsList;
+    }
+
+    public void setCreatedEventsList(ArrayList createdEventsList) {
+        this.createdEventsList = createdEventsList;
+    }
+
+    public
 }
